4254e2c458c33ca637f23793f4316c9a635c4044
Legrain Samuel <samuel.legrain@bstorm.be>
2021-08-04 13:09:08 +0200

    Chrono Exercice

--- src/app/exercice/exercice-routing.module.ts
+++ src/app/exercice/exercice-routing.module.ts
@@ -1,9 +1,12 @@
+import { ChronoComponent } from './Components/chrono/chrono.component';
 import { HomeComponent } from './Components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'', component : HomeComponent}
+  {path:'', component : HomeComponent, children : [
+    {path:'chrono',component : ChronoComponent},
+  ]},
 ];
 
 @NgModule({
--- src/app/exercice/exercice.module.ts
+++ src/app/exercice/exercice.module.ts
@@ -1,17 +1,21 @@
+import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ExerciceRoutingModule } from './exercice-routing.module';
 import { HomeComponent } from './Components/home/home.component';
+import { ChronoComponent } from './Components/chrono/chrono.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ChronoComponent
   ],
   imports: [
     CommonModule,
-    ExerciceRoutingModule
+    ExerciceRoutingModule,
+    SharedModule
   ],
   bootstrap : [HomeComponent]
 })
